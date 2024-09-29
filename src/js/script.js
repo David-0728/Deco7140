fff48c0f7d3c317e4ee0c973aa44a5d6a3b27f2e
David-0728 <s4778459@student.uq.edu.au>
2024-09-29 17:04:25 +1000

    s

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript to toggle visibility of elements
-function toggleVisibility(elementId) {
-    var element = document.getElementById(elementId);
-    if (element.style.display === 'none') {
-        element.style.display = 'block'; // Show the element
-    } else {
-        element.style.display = 'none'; // Hide the element
-    }
-}
