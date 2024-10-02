046fc401fb3f9cfbc12538a5af90f3ce1881725f
David-0728 <s4778459@student.uq.edu.au>
2024-10-02 16:54:23 +1000

    adding weather API

---
+++ Practice/js/general_modules.js
@@ -0,0 +1,9 @@
+const weatherURL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m";
+
+async function getWeather() {
+    let response = await fetch(weatherURL);
+    let data = await response.json();
+    return data;
+}
+
+export { getWeather };
--- Practice/js/project2script.js
+++ Practice/js/project2script.js
@@ -1,10 +1,8 @@
-const weatherURL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m";
+import { getWeather } from "./general_modules";
+displayWeather();
 
-async function getWeather() {
-    let response = await fetch(weatherURL);
-    let data = await response.json();
-    return data;
+async function displayWeatherWeather() {
+    let currentWeather = await getWeather();
+    console.log(currentWeather.current.temperature_2m);
 }
 
-console.log("Retrieving weather data");
-console.log(await getWeather());
